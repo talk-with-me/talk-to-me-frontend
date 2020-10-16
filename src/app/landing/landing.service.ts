bef0800e937592ead467b2a68e67aec3138178e1
Andrew Zhu <me@andrew-zhu.com>
2020-10-16 08:56:52 -0700

    chore: create service frameworks

---
+++ src/app/landing/landing.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LandingService {
+
+  constructor() {
+  }
+
+  enterQueue() {
+    // todo make request to enter queue
+  }
+}
