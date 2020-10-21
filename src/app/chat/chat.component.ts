3bc745efc294347e8f9f750e63d4298e0003fcd0
Andrew Zhu <me@andrew-zhu.com>
2020-10-21 12:48:37 -0700

    cleanup

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AppService} from '../app.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {MsgItem, WhateverItem} from './msg-item/msg-item';
+import {WhateverItem} from './msg-item/msg-item';
 
 
 @Component({
@@ -10,29 +9,32 @@ import {MsgItem, WhateverItem} from './msg-item/msg-item';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  @Output() submit: EventEmitter<string> = new EventEmitter();
-
-  default_text = "Type something...";
+  msgList: WhateverItem[] = [
+    {type: 'joinleave', isJoin: true},
+    {type: 'message', msg: 'string', sent: true, nonce: 'blah', sentByMe: true, id: 'asdf'},
+    {type: 'message', msg: 'second message', sent: true, nonce: 'blah', sentByMe: false, id: 'asdfa'},
+    {
+      type: 'message',
+      msg: 'This is a very long message, testing how it handles when the user types a really long message into the chat. This message has many characters',
+      sent: true,
+      nonce: 'blah',
+      sentByMe: false,
+      id: 'asdfasd'
+    },
+    {type: 'message', msg: 'this one is still sending', sent: false, nonce: 'blah', sentByMe: true, id: 'asdasdadf'},
+    {type: 'joinleave', isJoin: false},
+  ];
 
   constructor(private service: AppService) {
     // example implementation
     this.service.messageEmitter.subscribe(msg => this.onMessage(msg));
   }
 
-  ngOnInit() { }
-
-  msgList: WhateverItem[] = [
-    {type: 'joinleave', isJoin: true},
-    {type: 'message', msg: 'string', sent: true, nonce: 'blah', sentByMe: true, id: 'asdf'},
-    {type: 'message', msg: 'second message', sent: true, nonce: 'blah', sentByMe: false, id: 'asdfa'},
-    {type: 'message', msg: 'This is a very long message, testing how it handles when the user types a really long message into the chat. This message has many characters' ,
-      sent: true, nonce: 'blah', sentByMe: false, id: 'asdfasd'},
-    {type: 'message', msg: 'this one is still sending', sent: false, nonce: 'blah', sentByMe: true, id: 'asdasdadf'},
-    {type: 'joinleave', isJoin: false},
-  ]
+  ngOnInit() {
+  }
 
   sendMsg(msg: string) {
-    const obj = { msg , sent: false, nonce: 'blah', sentByMe: true};
+    const obj = {msg, sent: false, nonce: 'blah', sentByMe: true};
     // this.msgList.push(obj);
     // this.service.sendMessage(someRoomId, obj.msg, obj.nonce)
   }
