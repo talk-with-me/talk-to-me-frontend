b777985c8b5e5f18e6d77ac99eb69d139825ac83
Duncan Bark <duncanbark@me.com>
2020-10-21 08:40:10 -0700

    updated app module to use new MsgItem and SendChat components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LandingComponent} from './landing/landing.component';
 import {ChatComponent} from './chat/chat.component';
+import { MsgItemComponent } from './chat/msg-item/msg-item.component';
+import { SendChatComponent } from './chat/send-chat/send-chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    ChatComponent
+    ChatComponent,
+    MsgItemComponent,
+    SendChatComponent
   ],
   imports: [
     BrowserModule,
