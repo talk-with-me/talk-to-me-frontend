e0657c278dd137ac632f530c1929b9d417a3e548
Andrew Zhu <me@andrew-zhu.com>
2020-10-30 08:20:20 -0700

    add utils to remember popup state

---
+++ src/app/utils/popupUtils.ts
@@ -0,0 +1,9 @@
+const TTM_HAS_SEEN_POPUP_KEY = 'ttm-has-seen-popup';
+
+export function hasSeenPopup(): boolean {
+  return localStorage.getItem(TTM_HAS_SEEN_POPUP_KEY) !== null;
+}
+
+export function rememberSeenPopup(): void {
+  localStorage.setItem(TTM_HAS_SEEN_POPUP_KEY, true);
+}
