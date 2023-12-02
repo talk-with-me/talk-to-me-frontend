3d8b0eea43f3619712f4de867fde0057d501f629
Alexander Kostin <arkostin01@gmail.com>
2023-12-01 20:01:51 -0800

    Update landing page

--- src/Welcome.js
+++ src/Welcome.js
@@ -23,6 +23,9 @@ const useStyles = makeStyles((theme) => ({
     'text-align': 'center',
     'font-size': '72pt',
     'min-width': '500px',
+    'animation': '$fadeInTitle ease 2000ms',
+    'animation-delay': '500ms',
+    'animation-fill-mode': 'both',
     [theme.breakpoints.down('sm')]:{
       'font-size': '36pt',
     },
@@ -30,34 +33,78 @@ const useStyles = makeStyles((theme) => ({
       'font-size': '24pt',
     },
   },
+  "@keyframes fadeInTitle": {
+    "0%": {
+      opacity: 0,
+      transform: "translateY(-70%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0%)",
+    }
+  },
   mainBlurb: {
     'padding-top': '40px',
     'color': themeRed,
     'width': '800px',
     'text-align': 'center',
+    'animation': '$fadeInMainBlurb ease 2000ms',
+    'animation-delay': '1500ms',
+    'animation-fill-mode': 'both',
+  },
+  "@keyframes fadeInMainBlurb": {
+    "0%": {
+      opacity: 0,
+      transform: "translateY(-40%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0%)",
+    }
   },
   welcomeAndQuestions: {
     'display': 'flex',
     'flex-wrap': 'wrap',
   },
   qAndASection: {
-    'padding-top': '320px',
+    'padding-top': '40px',
     'font-size': '24pt',
+    'padding-bottom': '80px',
+    'animation': '$fadeInQAndA ease 2000ms',
+    'animation-delay': '2000ms',
+    'animation-fill-mode': 'both',
+    'max-width': '90vw'
+  },
+  "@keyframes fadeInQAndA": {
+    "0%": {
+      opacity: 0,
+      transform: "translateY(-10%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0%)",
+    }
   },
   oneQAndA: {
     'display': 'flex',
-    'min-height': '160px',
+    'min-height': '100px',
+  },
+  lastQAndA: {
+    'display': 'flex',
   },
   question: {
     'color': themeOrange,
-    'flex': '1',
+    'flex': '2',
     'text-align': 'end',
     'padding': '10px',
+    'padding-right': '20px',
+    'border-right': '2px solid ' + themeOrange
   },
   answer: {
     'color': themeRed,
-    'flex': '1',
+    'flex': '3',
     'padding': '10px',
+    'padding-left': '20px',
   },
 }));
 
@@ -100,7 +147,7 @@ function Welcome() {
           What is Talk To Me?
         </div>
         <div className={classes.answer}>
-          Talk To Me is an anonymous text chat service.
+          Talk To Me is an anonymous peer-to-peer text chat service.
         </div>
       </div>
       <div className={classes.oneQAndA}>
@@ -108,15 +155,18 @@ function Welcome() {
           What is the purpose of Talk To Me?
         </div>
         <div className={classes.answer}>
-          The purpose of Talk To Me is to give people a way to express their emotions and interact with someone anonymous, to provide positivity to their life.
+          Talk To Me was created as a place for people to come to, to just talk to someone else. During the COVID-19 pandemic,
+          many people experienced feelings of isolation that be could have been helped if they just had someone to talk to. Talk To Me was 
+          created to provide that.
         </div>
       </div>
-      <div className={classes.oneQAndA}>
+      <div className={classes.lastQAndA}>
         <div className={classes.question}>
-          Is Talk To Me a therapy service?
+          How does it work?
         </div>
         <div className={classes.answer}>
-          No, we do not recommend using Talk To Me in place of therapy. 
+          Click the button in the top right to be placed in the matchmaking queue. When a chat partner has been found, you will
+          be placed into a chatroom with said person. Be nice!
         </div>
       </div>
     </div>
